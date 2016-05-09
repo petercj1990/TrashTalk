d2f13e229248c731649bbb8ba6aab3d67d4c97da
Alex C Andrews <alexcandrews91@gmail.com>
2016-05-09 17:29:53 -0400

    display hello world html page

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var app = express();
+var path = require('path');
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.sendFile(path.resolve("index.html"));
 });
 
 app.listen(80, function () {
